7d3d3b2319960ec863420fa17eced55e051e31c8
Mihai405 <mihaijurj2000@yahoo.com>
2022-05-31 23:46:14 +0300

    Cors settings

--- server.js
+++ server.js
@@ -9,6 +9,25 @@ const port = process.env.PORT;
 
 app.use(cors());
 
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:4000",
+  "https://dashboard.heroku.com/apps/gympadapp/settings",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 const clientsRouter = require("./routes/clients");

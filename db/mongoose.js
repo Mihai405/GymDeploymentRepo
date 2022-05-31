7e6de3a637eb3aad8ae826f9a9531555ecb7e372
Mihai405 <mihaijurj2000@yahoo.com>
2022-05-31 23:54:18 +0300

    remove dotenv

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+//require("dotenv").config();
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+//require("dotenv").config();
 const mongoose = require("mongoose");
 const npmValidator = require("validator");
 const bcrypt = require("bcryptjs");
